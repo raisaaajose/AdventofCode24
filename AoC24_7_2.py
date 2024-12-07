0f7cbe939e254b39a500072fd8b01ac46d1c9694
raisaaajose <168289547+raisaaajose@users.noreply.github.com>
2024-12-07 22:19:32 +0530

    Create AoC24_7_2.py
    7.1 but including string addition along with integer addition and integer multiplication

---
+++ AoC24_7_2.py
@@ -0,0 +1,57 @@
+def check(operandslist,length,out):
+    if length==1:
+        
+        if operandslist[0]==str(out):
+            
+            return 1
+        else:
+            
+            return 0
+    if length>1:
+        
+        ele=int(operandslist[0])
+        sum=ele+int(operandslist[1])
+        prod=ele*int(operandslist[1])
+        concat=str(ele)+operandslist[1]
+        length-=1
+        
+        operandslistsum=operandslist.copy()
+        operandslistsum[1]=str(sum)
+        operandslistsprod=operandslist.copy()
+        operandslistsprod[1]=str(prod)
+        operandslistconcat=operandslist.copy()
+        operandslistconcat[1]=concat
+        
+        return check(operandslistsum[1:],length,out) + check(operandslistsprod[1:],length,out)+check(operandslistconcat[1:],length,out)
+        
+        
+    
+    
+    
+    
+
+main=[]
+while True:
+    s=input()
+    if not s:
+        break
+    else:
+        main.append(s)
+count=0
+for i in main:
+        arr=i.split(":")
+        output=int(arr[0])
+        
+        operandslist=arr[1].strip().split(" ")
+        length=len(operandslist)
+        if check(operandslist,length,output)>0:
+            
+            
+            count+=output
+        
+print(count)
+       
+        
+
+            
+
