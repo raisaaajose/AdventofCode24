0cf24464bda0cfdb2ac300459073b914dfee8c13
raisaaajose <168289547+raisaaajose@users.noreply.github.com>
2024-12-10 23:40:12 +0530

    Create AoC_9_1.py

---
+++ AoC_9_1.py
@@ -0,0 +1,49 @@
+arr=[]
+while True:
+    s=input()
+    if not s:
+        break
+    else:
+        arr.append(s)
+nines=[]
+def check(i,j,n):
+    if arr[i][j]=="9":
+        if [i,j] not in nines:
+            nines.append([i,j])
+        return 1
+    ls=0
+    rs=0
+    us=0
+    ds=0
+    try:
+        if j-1 >=0 and arr[i][j-1]==str(n+1):
+            ls=check(i,j-1,n+1)
+    except:
+        pass
+    try:
+        if arr[i][j+1]==str(n+1):
+            rs=check(i,j+1,n+1)
+    except:
+        pass
+    try:
+        if arr[i+1][j]==str(n+1):
+            ds=check(i+1,j,n+1)
+    except:
+        pass
+    try:
+        if i-1>=0 and arr[i-1][j]==str(n+1):
+            us=check(i-1,j,n+1)
+    except:
+        pass
+    # print(arr[i][j],i,j,ls,rs,us,ds)
+    return ls+ds+us+rs
+
+
+sum=0
+for i in range(len(arr)):
+    for j in range(len(arr[0])):
+        if arr[i][j]=="0":
+            nines=[]
+            check(i,j,0)
+            sum+=len(nines)
+print(sum)
